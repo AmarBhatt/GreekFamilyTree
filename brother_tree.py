3aa341670262c5a2dbd0d878b70cb80a05423905
Amar Bhatt <bhatt.amar.a@gmail.com>
2015-10-03 00:20:01 -0400

    Revert "Revert "read from roll""
    This reverts commit 77a622e29ced2651540a54fa78050baa0f97d7a4.

--- brother_tree.py
+++ brother_tree.py
@@ -1,20 +1,37 @@
+import csv
+
 class node:
-    number = 0
-    little=[]
+    def __init__(self, big, active, pledgeClass, roll, name):
+        self.roll = roll
+        self.name = name
+        self.big = big
+        self.active = active
+        self.pledgeClass = pledgeClass
+    def toString(self):
+        print(str(self.roll) + "\t" + self.name + "\t" + str(self.active) + "\t" + self.pledgeClass + "\t" + self.big)
 
 
-raw = open('Roll - Brothers.csv','r')
 
-count = 0
-for line in raw:
-    if count != 0:        
-        item = line.split(",",2)
-        '''item[0] = int(item[0])
-        item[2] = item[2].rstrip('\n')
-        item[2] = item[2].rstrip('"')'''
-        print(item)
-    count = count + 1
+filterOutMember = ['S','D']
+setActive = {"Active":0,"Alumni":1,"Disassociated":2, "Transferred/ Left School":1}
 
+with open('Roll - Brothers.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
 
+    count = 0
+    nodeList = []
+    for row in reader:
+        if count != 0:
+            if (not row[0][0] in filterOutMember):
+                 row[2] = setActive.get(row[2])
+                 n = node(row[5],row[2],row[6],int(row[0]),row[1])
+                 nodeList.append(n)
+                 n.toString()
+        count = count + 1
 
     
+
+
+
+        
+
