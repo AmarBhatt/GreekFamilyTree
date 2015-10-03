888ec259ae540c3d95ec106a74dbcc500e919609
Amar Bhatt <bhatt.amar.a@gmail.com>
2015-10-02 22:20:47 -0400

    Initial Commit
    File Reading Code and Test File

---
+++ brother_tree.py
@@ -0,0 +1,20 @@
+class node:
+    number = 0
+    little=[]
+
+
+raw = open('tree.csv','r')
+
+count = 0
+for line in raw:
+    if count != 0:        
+        item = line.split(",",2)
+        item[0] = int(item[0])
+        item[2] = item[2].rstrip('\n')
+        item[2] = item[2].rstrip('"')
+        print(item)
+    count = count + 1
+
+
+
+    
